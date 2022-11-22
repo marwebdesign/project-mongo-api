90ab4517740d715ab9441393caedc7c271b2a933
Maria Westling <maria_westling@hotmail.com>
2022-11-22 19:38:34 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+import eurovisionData from './data/eurovision-winners.json'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
+const Winner = mongoose.model("Winner", {
+  year: Number,
+  country: String,
+  song: String,
+  artist: String,
+  songwriters: String,
+  total_points: Number,
+  location: String
+})
+
+if (process.env.RESET_DB) {
+  const resetDataBase = async () => {
+    await Winner.deleteMany()
+    eurovisionData.forEach(singleWinner => {
+      const newWinner = new Winner(singleWinner)
+      newWinner.save()
+    })
+  }
+  resetDataBase()
+}
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
